118da699d1978c375360097e4b10794a1631a135
Philliphick <81019680+Philliphick@users.noreply.github.com>
2024-01-31 16:38:17 +0000

    styled

--- components/Add.js
+++ components/Add.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 const Add = (props) => {
     const [disabled, setDisabled] = useState(false);
+    const [hoveredIndex, setHoveredIndex] = useState(null);
 
     const submitHandler = (e) => {
         // stop the page from refreshing when the user submits the form. That is the default behaviour of HTML forms
@@ -31,13 +32,23 @@ const Add = (props) => {
         })
     }
   return (
-    <div>
-        <h2>Add new plant</h2>
+    // <div
+    //           key={index}
+    //           className={`flex flex-col px-10  ${hoveredIndex === index ? 'bg-gradient-to-br from-blue-100 to-blue-200' : ''}`}
+    //           onMouseEnter={() => setHoveredIndex(index)}
+    //           onMouseLeave={() => setHoveredIndex(null)}
+    //         >
+    <div className="top-0 right-0 fixed p-4 rounded-lg text-center flex flex-col bg-slate-200 opacity-80">
+        
+        <h2 className="text-2xl p-6 font-bold">Add New Plant</h2>
         <form onSubmit={submitHandler} id="addForm">
-            Name: <input type="text" name="name" placeholder="Enter plant name here" disabled={disabled} />
-            <button type="submit" disabled={disabled}>Add</button>
+           <div className="flex flex-row gap-4">
+           <p> Name: </p><input type="text" name="name" placeholder="Enter plant name here" disabled={disabled} />
+           </div>
+            <button type="submit" disabled={disabled} className="bg-cyan-400 text-white rounded m-4 p-2 hover:bg-cyan-900">Add</button>
         </form>
     </div>
+    // </div>
   )
 }
 
